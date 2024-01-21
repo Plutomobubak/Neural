6e93ce35966000296f52a69ed1e296f2816a707a
Plutomobubak <122475076+Plutomobubak@users.noreply.github.com>
2024-01-21 21:28:50 +0100

    car raycasting

--- sensor.js
+++ sensor.js
@@ -45,7 +45,8 @@ class Sensor{
         for(let i=0;i<traffic.length;i++){
             const poly=traffic[i].polygon
             for(let j=0;j<poly.length;j++){
-                //finish
+                const value=getIntersection(ray[0],ray[1],poly[j],poly[(j+1)%poly.length])
+                if(value)touches.push(value)
             }
         }
         if(touches.length==0){
